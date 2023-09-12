23c2b5917c7d85f89378a17ce6af34d205758583
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-12 17:58:38 +0200

    added popper for bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
 import { router } from './router';
 import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 createApp(App).use(router).mount('#app');
