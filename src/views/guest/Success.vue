ac03b29940ea7827df2f65c3101bb1ff2ed06fc4
simonescoca <simone.scoca@icloud.com>
2023-09-13 13:05:09 +0200

    added view

---
+++ src/views/guest/Success.vue
@@ -0,0 +1,42 @@
+<template>
+    <h3>
+        Succes
+    </h3>
+</template>
+
+<script>
+	// import {store} from "../store.js";
+	// import axios from "axios";
+
+	export default {
+		data() {
+			return {
+				// store
+			}
+		},
+
+		components: {
+
+		},
+
+		props: {
+
+		},
+
+		mounted () {
+
+		},
+
+		created () {
+
+		},
+
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
