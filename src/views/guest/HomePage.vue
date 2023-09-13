4f1d1307eba52ecd941a0ca6f125f7cba8250648
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-13 12:55:00 +0200

    added how it works section to the homepage with collapse

--- src/views/guest/HomePage.vue
+++ src/views/guest/HomePage.vue
@@ -24,6 +24,40 @@
 			</div>
 		</div>
 	</div>
+	<!-- ---How it works section--- -->
+	<div class="container">
+        <div class="row border-top border-bottom py-3">
+			<div class="col">
+				<h3>How it works</h3>
+			</div>
+			<div class="col-1">
+				<!-- Bottone per aprire/chudere la section -->
+				<button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#howItWorks" aria-expanded="true" aria-controls="howItWorks">
+					<i class="fas fa-chevron-down"></i>
+				</button>
+			</div>
+		</div>
+        <!-- Section "How it works" con Collapse -->
+        <div class="collapse show" id="howItWorks">
+            <div class="row">
+                <div class="col-12 col-md-4">
+					<img src="../../images/stepone.png" alt="step one image" class="w-75 m-5">
+					<h3>Step 1</h3>
+					<p>Enter your location. Input your address or enable location services to pin your location.</p>
+				</div>
+				<div class="col-12 col-md-4 border-start border-end">
+					<img src="../../images/steptwo.png" alt="step two image" class="w-75 m-5">
+					<h3>Step 2</h3>
+					<p>Select a restaurant and add items to your cart. You're one step closer to a satisfying meal.</p>
+				</div>
+				<div class="col-12 col-md-4">
+					<img src="../../images/stepthree.png" alt="step three image" class="w-75 m-5">
+					<h3>Step 3</h3>
+					<p>Complete your payment. Sit back and relax as we prepare your order!</p>
+				</div>
+            </div>
+        </div>
+    </div>
 	
 </template>
 
