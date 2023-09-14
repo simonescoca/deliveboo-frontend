ab06c71f81f8ce6deac1a60ac7f749d78a5a7461
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-14 17:19:27 +0200

    added register section with img and signup area

--- src/views/guest/SigninLogin.vue
+++ src/views/guest/SigninLogin.vue
@@ -1,22 +1,34 @@
 <template>
-    <h3>
-        Sign-in / Login
-    </h3>
+	<!-- ---Register section, active when store access is false--- -->
+	<div class="container register" v-if="store.access === false">
+		<div class="row my-5">
+			<!-- ---Register img--- -->
+			<div class="col-5 d-flex">
+				<img src="../../images/register.png" alt="" class="w-100 m-auto">
+			</div>
+			<!-- ---Form Register--- -->
+			<div class="col-7 text-center">
+				<h1>Sign up</h1>
+				<h5>Enter your information to get started</h5>
+				
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-	// import {store} from "../store.js";
-	// import axios from "axios";
+	import {store} from "../../store";
+	import axios from "axios";
 
 	export default {
 		data() {
 			return {
-				// store
+				store,
 			}
 		},
 
 		components: {
-
+			
 		},
 
 		props: {
@@ -38,5 +50,7 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../../styles/variables' as *;
+
 
 </style>
