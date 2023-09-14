7fe7fbc669dc8d47a336c3ea487af1f2a13cecc5
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-14 17:43:43 +0200

    Created checkout view

---
+++ src/views/guest/CheckOut.vue
@@ -0,0 +1,43 @@
+<template>
+    <h3>
+        Check Out
+    </h3>
+</template>
+
+<script>
+	// import {store} from "../store.js";
+	// import axios from "axios";
+
+	export default {
+		name: 'CheckOut',
+		data() {
+			return {
+				// store
+			}
+		},
+
+		components: {
+
+		},
+
+		props: {
+
+		},
+
+		mounted () {
+
+		},
+
+		created () {
+
+		},
+
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
