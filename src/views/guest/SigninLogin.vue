02c10581b36422b7e3535eda4013f76a4637f563
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-14 17:29:48 +0200

    added login with fb button with animation

--- src/views/guest/SigninLogin.vue
+++ src/views/guest/SigninLogin.vue
@@ -142,6 +142,19 @@
 					<!-- ---Need to register--- -->
 					<p class="alreadyLogin">New to Deliveboo? <a href="#" class="fw-semibold" @click="store.access = false">Create an account</a></p>
 				</form>
+				<h5>OR</h5>
+				<div class="orline mx-auto">
+					<div></div>
+				</div>
+				<!-- ---Login with facebook button--- -->
+				<div class="d-flex flex-column align-items-center mt-3">
+					<button class="facebookbtn">
+						<svg viewBox="0 0 16 16" class="bi bi-facebook" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+							<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+						</svg>
+						<span>Connect with Facebook</span>
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -463,5 +476,60 @@
 		}
 	}
 	
+
+// ---Facebook button---
+	.facebookbtn {
+		background: transparent;
+		position: relative;
+		padding: 5px 15px;
+		display: flex;
+		align-items: center;
+		font-size: 17px;
+		font-weight: 600;
+		text-decoration: none;
+		cursor: pointer;
+		border: 1px solid #0163E0;
+		border-radius: 25px;
+		outline: none;
+		overflow: hidden;
+		color: #0163E0;
+		transition: color 0.3s 0.1s ease-out;
+		text-align: center;
+		svg {
+			fill: #0163E0;
+			height: 25px;
+			width: 25px;
+		}
+		span {
+			margin: 10px;
+		}
+		&::before {
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			margin: auto;
+			content: '';
+			border-radius: 50%;
+			display: block;
+			width: 20em;
+			height: 20em;
+			left: -3rem;
+			text-align: center;
+			transition: box-shadow 0.5s ease-out;
+			z-index: -1;
+		}
+		&:hover {
+			color: #fff;
+			border: 1px solid #0163E0;
+		}
+		&:hover svg {
+			fill: currentColor;
+		}
+		&:hover::before {
+			box-shadow: inset 0 0 0 10em #0163E0;
+		}
+	}
 
 </style>
