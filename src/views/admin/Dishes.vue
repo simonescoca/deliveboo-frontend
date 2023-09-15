b57573d016ba3d72fee6d0b9a4a21857b88b8025
simonescoca <simone.scoca@icloud.com>
2023-09-15 17:05:42 +0200

    added index of dishes in the show of a single restaurant

--- src/views/admin/Dishes.vue
+++ src/views/admin/Dishes.vue
@@ -1,7 +1,22 @@
 <template>
-    <h3>
-        Admin - My Foods
+    <h3 class="container">
+        I piatti del tuo ristorante "nomeRistorante"
     </h3>
+    <div class="d-flex flex-wrap my_dishes">
+        <div v-for="dish in dishes" class="position-relative my_dish">
+            <div class="my_r-img">
+                <img :src="dish.image" :alt="dish.name">
+            </div>
+            <div class="position-absolute top-0 start-0 text-white fs-4 fw-bold p-2 my_r-name">
+                {{ dish.name }}
+            </div>
+            <div class="d-flex">
+            </div>
+            <router-link to="{ name: crud-food }" class="btn btn-warning">
+                Dishes
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
