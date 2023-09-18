ba39a66e958f76e2f7bdeaebaa1011cb3f9c9efb
simonescoca <simone.scoca@icloud.com>
2023-09-18 13:31:27 +0200

    added view with a form to edit a dish

--- src/views/admin/EditDish.vue
+++ src/views/admin/EditDish.vue
@@ -1,7 +1,28 @@
 <template>
-    <h3>
-        Admin - Add or Edit a Food
-    </h3>
+    <div class="container">
+        <h3>
+            Admin - Edit Dish
+        </h3>
+        <form>
+            <div v-for="formSection in formSections" class="mb-3">
+                <label :for="formSection.labelFor" class="form-label">
+                    {{ formSection.labelContent }}
+                </label>
+                <input type="text" class="form-control" :id="formSection.inputID" :aria-describedby="formSection.labelFor">
+            </div>
+            <div class="d-flex">
+                <div v-for="formCheck in formChecks" class="mb-3 ms-4 form-check">
+                    <input type="checkbox" class="form-check-input" :id="formCheck">
+                    <label class="form-check-label" for="cuisine-type">
+                        {{'Cucina ' + formCheck }}
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">
+                Update
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
