413c8642c3abe25e34280af5a3ba830845558965
simonescoca <simone.scoca@icloud.com>
2023-09-18 16:53:17 +0200

    added edit view for dishes

--- src/views/admin/EditDish.vue
+++ src/views/admin/EditDish.vue
@@ -5,19 +5,51 @@
         </h3>
         <form>
             <div v-for="formSection in formSections" class="mb-3">
-                <label :for="formSection.labelFor" class="form-label">
-                    {{ formSection.labelContent }}
-                </label>
-                <input type="text" class="form-control" :id="formSection.inputID" :aria-describedby="formSection.labelFor">
-            </div>
-            <div class="d-flex">
-                <div v-for="formCheck in formChecks" class="mb-3 ms-4 form-check">
-                    <input type="checkbox" class="form-check-input" :id="formCheck">
-                    <label class="form-check-label" for="cuisine-type">
-                        {{'Cucina ' + formCheck }}
+                <div v-if="formSection.labelFor != 'description'">
+                    <label :for="formSection.labelFor" class="form-label">
+                        {{ formSection.labelContent }}
+                    </label>
+                    <input :type="formSection.inputType" :class="formSection.inputClass" :id="formSection.inputID" :aria-describedby="formSection.labelFor">
+                </div>
+                <div v-else class="form-floating">
+                    <textarea class="form-control" :placeholder="formSection.textareaPlaceholder" :id="formSection.textareaID">
+
+                    </textarea>
+                    <label :for="formSection.labelFor" class="form-label">
+                        {{ formSection.labelContent }}
                     </label>
                 </div>
             </div>
+            <div class="form-check form-switch form-check-reverse">
+                <input class="form-check-input" type="checkbox" id="is-it-available">
+                <label class="form-check-label" for="is-it-available">
+                    is it available?
+                </label>
+            </div>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">
+                    Add image
+                </label>
+                <input class="form-control" type="file" id="image">
+            </div>
+            <select class="form-select" aria-label="select-course">
+                <option selected>
+                    Select course
+                </option>
+                <option v-for="course in courses" :value="course">
+                    {{ course }}
+                </option>
+            </select>
+
+            <label for="ingredients" class="form-label mt-4">
+                Type dish ingredients
+            </label>
+            <input type="text" class="form-control mb-3" id="ingredient" v-model="ingredient">
+            <div class="btn btn-success" @click="addIngredient">
+                Add Ingredient
+            </div>
+
+
             <button type="submit" class="btn btn-primary">
                 Update
             </button>
@@ -33,6 +65,39 @@
 		data() {
 			return {
 				// store
+                formSections: [
+                    {
+                        labelFor: 'name',
+                        labelContent: 'Nome',
+                        inputType: 'text',
+                        inputClass: 'form-control',
+                        inputID: 'name',
+                    },
+                    {
+                        labelFor: 'description',
+                        labelContent: 'Descrizione',
+                        textareaID: 'description',
+                        textareaPlaceholder: 'Inserici una descrizione del piatto...'
+                    },
+                    {
+                        labelFor: 'price',
+                        labelContent: 'Prezzo',
+                        inputType: 'number',
+                        inputClass: 'form-control',
+                        inputID: 'price',
+                    },
+                ],
+
+                courses: [
+                    'Antipasto',
+                    'Primo',
+                    'Secondo',
+                    'Contorno',
+                    'Dolce',
+                ],
+
+                ingredient: '',
+                ingredients: [],
 			}
 		},
 
@@ -53,7 +118,11 @@
 		},
 
 		methods: {
-
+            addIngredient () {
+                this.ingredients.push(this.ingredient)
+                console.log(this.ingredients)
+                this.ingredient = ''
+            }
 		}
 	}
 </script>
