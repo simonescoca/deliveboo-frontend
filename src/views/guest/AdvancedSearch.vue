1e23546a1f317dcd3ce3c32c1b0a625d2c338a2c
simonescoca <simone.scoca@icloud.com>
2023-09-19 19:32:54 +0200

    added advanced search

--- src/views/guest/AdvancedSearch.vue
+++ src/views/guest/AdvancedSearch.vue
@@ -1,17 +1,61 @@
 <template>
-    <h3>
-        Advanced Search
-    </h3>
+    <header class="mt-4 mb-3 bg-primary py-4">
+        <div class="d-flex justify-content-around align-items-center container">
+            <div>
+                Find your favorite
+            </div>
+            <div class="my_searchbar w-50">
+                <input type="text" v-model="restCategOrName" placeholder="Restaurant name or type" @input="getRestaurants(restCategOrName)">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </header>
+    <main class="d-flex flex-wrap justify-content-around align-items-center container">
+        <div v-for="initialRestaurant in initialRestaurants" class="mb-4">
+            <div class="card" style="width: 24rem;">
+                <img src="" class="card-img-top" :alt="initialRestaurant.name">
+                <div class="card-body">
+                <h5 class="card-title">
+                    {{ initialRestaurant.name }}
+                </h5>
+                <p class="card-text">
+                    {{ initialRestaurant.city + ", " + initialRestaurant.address }}
+                </p>
+                <div class="d-flex align-items-center my_categories">
+                    <p v-for="category in initialRestaurant.types" class="m-0 ms-2 mb-2 card-text">
+                        {{ category.name }}
+                    </p>
+                </div>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
 	// import {store} from "../store.js";
-	// import axios from "axios";
+	import axios from "axios";
 
 	export default {
 		data() {
 			return {
 				// store
+                restCategOrName: '',
+                apiUrl: 'http://127.0.0.1:8000/api',
+                initialRestaurants: [],
+                categories: [
+                    'Italiana',
+                    'Francese',
+                    'Giapponese',
+                    'Messicana',
+                    'Cinese',
+                    'Americana',
+                    'Indiano',
+                    'Mediterranea',
+                    'Mediorientale',
+                    'Vietnamita',
+                ],
 			}
 		},
 
@@ -28,15 +72,61 @@
 		},
 
 		created () {
-
+            this.getRestaurants();
 		},
 
 		methods: {
+            getRestaurants(search){
+                axios.get(`${this.apiUrl}/restaurants`,{
+                    params: {
+                        restCategOrName: this.restCategOrName,
+                    }
+                })
+                .then(response => {
+                    if (search === '') {
+                        this.initialRestaurants = response.data.results.data
+                    } else {
+                        this.initialRestaurants = []
+                        
+                        response.data.results.data.forEach( (restaurant) => {
+                            if (restaurant.name.includes(search)) {
+                                this.initialRestaurants.push(restaurant)
+                            }
+                        })
 
+                        response.data.results.data.forEach( (restaurant) => {
+                            restaurant.types.forEach ( (category) => {
+                                if (category.name.includes(search)) {
+                                    this.initialRestaurants.push(restaurant)
+                                }
+                            })
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+            },
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+
+    @use '../../styles/variables' as *;
+    .my_searchbar {
+        background-color: $grey;
+        padding: 10px 5px;
+        border-radius: 8px;
+        input{
+            background-color: $grey;
+            border: none;
+            width: 90%;
+            &:focus-visible{ 
+                outline: none;
+                box-shadow: none;
+            }
+        }
+    }
 
 </style>
