d5a95ce98b9c3c8b2f0bb936d989bd73e463b7f2
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-09-21 12:55:42 +0200

    added axios call on mount of the page to get restaurant info and dishes

--- src/views/guest/RestaurantMenu.vue
+++ src/views/guest/RestaurantMenu.vue
@@ -5,17 +5,18 @@
 	<!-- ---Sezione selezione sottocategorie--- -->
 
 	<!-- ---Sezione piatti, divisa in Primi, Secondi, Dolci--- -->
-	
+
 </template>
 
 <script>
-	// import {store} from "../store.js";
-	// import axios from "axios";
+	import {store} from "../../store.js";
+	import axios from "axios";
 
 	export default {
 		data() {
 			return {
-				// store
+				store,
+				apiUrl: 'http://127.0.0.1:8000/api/',
 			}
 		},
 
@@ -28,7 +29,7 @@
 		},
 
 		mounted () {
-
+			this.getRestaurantInfo()
 		},
 
 		created () {
@@ -36,7 +37,15 @@
 		},
 
 		methods: {
-
+			getRestaurantInfo() {
+            axios.get(`${this.apiUrl}restaurants/1`)
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+			},
 		}
 	}
 </script>
