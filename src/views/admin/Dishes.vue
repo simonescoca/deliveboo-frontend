e5f6c4722c31dd4ea5fabb2c2dba771ba319135c
manuelcamaioni <camaioni.manuel@gmail.com>
2023-09-21 20:04:53 +0200

    added image to dishes

--- src/views/admin/Dishes.vue
+++ src/views/admin/Dishes.vue
@@ -17,7 +17,7 @@
         <div class="d-flex flex-wrap my_dishes">
             <div v-for="dish in dishes" class="position-relative my_dish">
                 <div class="my_r-img">
-                    <img :src="dish.image" :alt="dish.name">
+                    <img :src="dish.photo" :alt="dish.name">
                 </div>
                 <div class="position-absolute top-0 start-0 text-white fs-4 fw-bold p-2 my_r-name">
                     {{ dish.name }}
