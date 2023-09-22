e4d6a1f73f26693d46daa5b49aebe8abd12a7117
manuelcamaioni <camaioni.manuel@gmail.com>
2023-09-22 16:37:52 +0200

    restyled form add dish

--- src/views/admin/AddDish.vue
+++ src/views/admin/AddDish.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
         <h3>
-            Admin - Add a Dish
+            Admin - Crea un piatto
         </h3>
         <div v-if="isUpdateSuccess" class="alert alert-success">
-           La creazione del tuo piatto è andata a buon fine!
+            La creazione del tuo piatto è andata a buon fine!
         </div>
         <div v-if="isUpdateFailure" class="alert alert-danger">
-           La creazione del tuo piatto non è andata a buon fine. Si è verificato un errore.
+            La creazione del tuo piatto non è andata a buon fine. Si è verificato un errore.
         </div>
         <form @submit.prevent="createDish">
             <div v-for="formSection in formSections" class="mb-3">
@@ -15,10 +15,12 @@
                     <label :for="formSection.labelFor" class="form-label">
                         {{ formSection.labelContent }}
                     </label>
-                    <input :type="formSection.inputType" :class="formSection.inputClass" :id="formSection.inputID" :aria-describedby="formSection.labelFor" v-model="newDish[formSection.inputID]">
+                    <input :type="formSection.inputType" :class="formSection.inputClass" :id="formSection.inputID"
+                        :aria-describedby="formSection.labelFor" v-model="newDish[formSection.inputID]">
                 </div>
                 <div v-else class="form-floating">
-                    <textarea class="form-control" :placeholder="formSection.textareaPlaceholder" :id="formSection.textareaID " v-model = "newDish.description">
+                    <textarea class="form-control" :placeholder="formSection.textareaPlaceholder"
+                        :id="formSection.textareaID" v-model="newDish.description">
 
                     </textarea>
                     <label :for="formSection.labelFor" class="form-label">
@@ -26,21 +28,21 @@
                     </label>
                 </div>
             </div>
-            <div class="form-check form-switch form-check-reverse">
+            <div class="form-check form-switch form-check my-3">
                 <input class="form-check-input" type="checkbox" id="is-it-available" v-model="newDish.available">
                 <label class="form-check-label" for="is-it-available">
-                    is it available?
+                    E' disponibile?
                 </label>
             </div>
             <div class="mb-3">
                 <label for="formFile" class="form-label">
-                    Add image
+                    Aggiungi immagine
                 </label>
-                <input class="form-control" type="text" id="formFile" v-model = newDish.photo>
+                <input class="form-control" type="text" id="formFile" v-model=newDish.photo>
             </div>
             <select class="form-select" aria-label="select-course" id="course" v-model="newDish.course">
                 <option selected>
-                    Select course
+                    Seleziona portata
                 </option>
                 <option v-for="course in courses" :value="course">
                     {{ course }}
@@ -48,157 +50,191 @@
             </select>
 
             <label for="ingredients" class="form-label mt-4">
-                Type dish ingredients
+                Scrivi un ingrediente
             </label>
             <input type="text" class="form-control mb-3" id="ingredient" v-model="ingredient">
-            <div class="btn btn-success" @click="addIngredient">
-                Add Ingredient
+            <div v-if="newDish.ingredients.length > 0" class="py-3">
+                <span class="me-2 pill-ingr" v-for="ingredientNewDish, index in newDish.ingredients"><i
+                        class="fa-solid fa-sm fa-xmark me-2" @click="removeFromArray(index)"></i>{{ ingredientNewDish
+                        }}</span>
             </div>
-            <select class="form-select" aria-label="select-course" id="type" v-model="newDish.type">
+
+            <div class="btn btn-success me-3" @click="addIngredient">
+                Aggiungi ingrediente
+            </div>
+            <div class="btn btn-secondary" @click="emptyIngredients">
+                Svuota
+            </div>
+            <select class="form-select my-3" aria-label="select-course" id="type" v-model="newDish.type">
                 <option selected>
-                    Select nationality of the dish
+                    {{ newDish.type }}
                 </option>
                 <option v-for="formCheck in formChecks" :value="formCheck">
                     {{ formCheck }}
                 </option>
             </select>
             <button type="submit" class="btn btn-primary">
-                create
+                Crea
             </button>
         </form>
     </div>
 </template>
 
 <script>
-	import {store} from "../../store.js";
-	 import axios from "axios";
+import { store } from "../../store.js";
+import axios from "axios";
 
-	export default {
-		data() {
-			return {
-				store,
-                apiUrl: 'http://127.0.0.1:8000/api/',
-				userToken: '',
-				userId: '',
-				userName: '',
-                isUpdateSuccess: false, 
-                isUpdateFailure: false,
-                formSections: [
-                    {
-                        labelFor: 'name',
-                        labelContent: 'Nome',
-                        inputType: 'text',
-                        inputClass: 'form-control',
-                        inputID: 'name',
-                    },
-                    {
-                        labelFor: 'description',
-                        labelContent: 'Descrizione',
-                        textareaID: 'description',
-                        textareaPlaceholder: 'Inserici una descrizione del piatto...'
-                    },
-                    {
-                        labelFor: 'price',
-                        labelContent: 'Prezzo',
-                        inputType: 'number',
-                        inputClass: 'form-control',
-                        inputID: 'price',
-                    },
-                ],
-                formChecks: [
-                    'Italiana',
-                    'Francese',
-                    'Giapponese',
-                    'Messicana',
-                    'Cinese',
-                    'Americana',
-                    'Mediterranea',
-                    'Mediorientale',
-                    'Vietnamita',
-                ],
-
-                courses: [
-                    'Antipasti',
-                    'Primi',
-                    'Secondi',
-                    'Contorni',
-                    'Dolci',
-                ],
-                newDish: {
-                    name: '',
-                    description:'',
-                    price:'',
-                    course:'',
-                    photo:'',
-                    available:false,
-                    ingredients: [],
-                    type:''
+export default {
+    data() {
+        return {
+            store,
+            apiUrl: 'http://127.0.0.1:8000/api/',
+            userToken: '',
+            userId: '',
+            userName: '',
+            isUpdateSuccess: false,
+            isUpdateFailure: false,
+            formSections: [
+                {
+                    labelFor: 'name',
+                    labelContent: 'Nome',
+                    inputType: 'text',
+                    inputClass: 'form-control',
+                    inputID: 'name',
                 },
-                ingredient: '',
-                
-			}
-		},
+                {
+                    labelFor: 'description',
+                    labelContent: 'Descrizione',
+                    textareaID: 'description',
+                    textareaPlaceholder: 'Inserici una descrizione del piatto...'
+                },
+                {
+                    labelFor: 'price',
+                    labelContent: 'Prezzo',
+                    inputType: 'number',
+                    inputClass: 'form-control',
+                    inputID: 'price',
+                },
+            ],
+            formChecks: [
+                'Italiana',
+                'Francese',
+                'Giapponese',
+                'Messicana',
+                'Cinese',
+                'Americana',
+                'Mediterranea',
+                'Mediorientale',
+                'Vietnamita',
+            ],
 
-		components: {
+            courses: [
+                'Antipasti',
+                'Primi',
+                'Secondi',
+                'Contorni',
+                'Dolci',
+            ],
+            newDish: {
+                name: '',
+                description: '',
+                price: '',
+                course: 'Seleziona portata',
+                photo: '',
+                available: false,
+                ingredients: [],
+                type: 'Seleziona la tipologia del piatto'
+            },
+            ingredient: '',
+            selectedRes: '',
 
-		},
+        }
+    },
 
-		props: {
+    components: {
 
-		},
+    },
 
-		mounted () {
+    props: {
 
-		},
+    },
 
-		created () {
-            this.userToken = localStorage.getItem('userToken')
-			this.userId = localStorage.getItem('userId')
-			this.userName = localStorage.getItem('userName')
-		},
+    mounted() {
+        this.selectedRes = localStorage.getItem('currentRestaurant');
+    },
 
-		methods: {
-            createDish(){
-                axios.post(`${this.apiUrl}${this.userId}/restaurants/${store.selectedRes}/dishes`,{
-                    name: this.newDish.name,
-                    description: this.newDish.description,
-                    price: this.newDish.price,
-                    course: this.newDish.course,
-                    photo:this.newDish.photo,
-                    available:this.newDish.available,
-                    ingredients:this.newDish.ingredients,
-                    type:this.newDish.type                   
-                },{
-                    headers: {
-                        'Authorization': `Bearer ${this.userToken}`
-                    }
-                })
+    created() {
+        this.userToken = localStorage.getItem('userToken')
+        this.userId = localStorage.getItem('userId')
+        this.userName = localStorage.getItem('userName')
+        if (store.selectedRes) {
+            localStorage.setItem('currentRestaurant', store.selectedRes);
+        }
+    },
+
+    methods: {
+        createDish() {
+            axios.post(`${this.apiUrl}${this.userId}/restaurants/${this.selectedRes}/dishes`, {
+                name: this.newDish.name,
+                description: this.newDish.description,
+                price: this.newDish.price,
+                course: this.newDish.course,
+                photo: this.newDish.photo,
+                available: this.newDish.available,
+                ingredients: this.newDish.ingredients,
+                type: this.newDish.type
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${this.userToken}`
+                }
+            })
                 .then(response => {
                     if (response.status === 200 || response.status === 204) {
-          
-                     this.isUpdateSuccess = true;
-                     this.isUpdateFailure = false; 
-                    } 
-                    
+
+                        this.isUpdateSuccess = true;
+                        this.isUpdateFailure = false;
+                    }
+
                     console.log(response)
-                    
+
 
                 })
                 .catch(error => {
                     console.error(error);
-                    this.isUpdateSuccess = false; 
+                    this.isUpdateSuccess = false;
                     this.isUpdateFailure = true;
                 });
-            },
-            addIngredient () {
+        },
+        addIngredient() {
+            if (this.ingredient != '') {
                 this.newDish.ingredients.push(this.ingredient)
                 console.log(this.newDish.ingredients)
                 this.ingredient = ''
             }
-		}
-	}
+        },
+        emptyIngredients() {
+            this.newDish.ingredients = [];
+            console.log(this.newDish.ingredients)
+        },
+        removeFromArray(index) {
+            this.newDish.ingredients.splice(index, 1);
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
+h3 {
+    border-bottom: 3px solid #ff9474;
+    border-radius: 5px;
+    text-align: center;
+    padding: 0.6rem 0;
+    margin: 1rem 0;
+}
 
+.pill-ingr {
+    background-color: rgb(189, 181, 181);
+    border-radius: 0.375rem;
+    padding: 0.3rem 0.5rem;
+}
 </style>
