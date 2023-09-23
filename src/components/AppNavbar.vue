009c6b0687149a3c8a9dff45308cba3ab625934b
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-23 15:08:25 +0200

    lightly fixed navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,6 +1,6 @@
 <template>
-	<nav class="position-relative">
-        <div class="container d-flex justify-content-between align-items-center">
+	<nav class="position-relative d-flex justify-content-center">
+        <div class=" d-flex justify-content-around align-items-center w-100">
             <!-- ? logo container -->
             <div class="d-flex align-items-center my_logo-container">
                 <img src="../../src/images/logo.jpeg" alt="logo">
