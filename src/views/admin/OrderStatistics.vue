162ef275e930f7a2b5ae7d944a5abb52f8d1bc96
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-09-27 18:43:40 +0200

    fixed OrderStatistics.vue component to display chart correctly

--- src/views/admin/OrderStatistics.vue
+++ src/views/admin/OrderStatistics.vue
@@ -2,29 +2,35 @@
     <h3>
         Admin - Orders' Statistics
     </h3>
+
+	<canvas id="myChart" width="400" height="200"></canvas>
+   
 </template>
 
 <script>
 	// import {store} from "../store.js";
 	// import axios from "axios";
-
+    import { initChart } from '../../orderStatistics.js';
 	export default {
 		data() {
 			return {
+				
 				// store
 			}
 		},
 
 		components: {
-
+           
 		},
 
 		props: {
-
+			
+			
 		},
 
 		mounted () {
-
+			this.chartInstance = initChart(this.$route.params.monthlySales, this.$route.params.orderCount);
+			console.log(this.$route.params.orderCount)
 		},
 
 		created () {
@@ -32,7 +38,7 @@
 		},
 
 		methods: {
-
+             
 		}
 	}
 </script>
